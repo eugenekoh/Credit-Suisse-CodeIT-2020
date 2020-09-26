e042fa7ab776a0609e6a1530bc1d45f661ad8479
Eugene Koh <ekoh016@e.ntu.edu.sg>
2020-09-26 20:27:33 +0800

    optimized-portfoltio:

--- codeitsuisse/tests/test_optimized_portfolio.py
+++ codeitsuisse/tests/test_optimized_portfolio.py
@@ -1,5 +1,5 @@
-from codeitsuisse.routes.optimized_portfolio import evaluate_optimized_portfolio
-
+from codeitsuisse.routes.optimized_portfolio import optimized_portfolio
+import pandas as pd
 
 def test_optimized_portfolio():
     data = {
@@ -8,12 +8,21 @@ def test_optimized_portfolio():
                 "Portfolio": {"Name": "Portfolio_Unhedged", "Value": 200000000, "SpotPrcVol": 0.75},
                 "IndexFutures":
                     [
-                        {"Name": "Index_Fut_A", "CoRelationCoefficient": 0.75, "FuturePrcVol": 0.95,
+                        {"Name": "Index_Fut_A", "CoRelationCoefficient": 0.1, "FuturePrcVol": 0.95,
                          "IndexFuturePrice": 20.5, "Notional": 100000},
                         {"Name": "Index_Fut_B", "CoRelationCoefficient": 0.25, "FuturePrcVol": 0.85,
+                         "IndexFuturePrice": 15.5, "Notional": 1000},
+                        {"Name": "Index_Fut_C", "CoRelationCoefficient": 0.25, "FuturePrcVol": 0.85,
                          "IndexFuturePrice": 15.5, "Notional": 100000}
                     ]
             }
         ]
     }
-    print(evaluate_optimized_portfolio(data))
+    d = data["inputs"][0]
+    portfolio_value = d["Portfolio"]["Value"]
+    spot_volatility = d["Portfolio"]["SpotPrcVol"]
+    df = pd.DataFrame(d["IndexFutures"])
+
+    result = optimized_portfolio(portfolio_value, spot_volatility, df)
+
+    print(print(result))
