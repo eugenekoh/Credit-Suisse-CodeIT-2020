6697f129a349bb7eeaaa35c54e901915abb230f5
hanzhe <sith0008@e.ntu.edu.sg>
2020-09-26 20:15:06 +0800

    social dist dp

--- codeitsuisse/routes/social_distance.py
+++ codeitsuisse/routes/social_distance.py
@@ -20,30 +20,46 @@ def evaluate_social_distance():
         seats = value.get("seats")
         people = value.get("people")
         spaces = value.get("spaces")
-        ans = find_social_distance(seats, people, spaces)
+        ans = find_social_distance(people, seats, spaces)
         response["answers"][key] = ans
     
     return jsonify(response)
 
-def find_social_distance(seats, people, spaces):
-    ans = 0    
-    seatArr = [False] * seats
-    def backtrack(seatsLeft, peopleLeft, spaces, arr, i):
-        if peopleLeft == 0:
-            nonlocal ans
-            ans += 1
-            return
-        if seatsLeft <= 0:
-            return
-        minusSeat = 1
-        arr[i] = True
-        for j in range(i+1, i+1+spaces):
-            if j < seats:
-                arr[j] = False
-                minusSeat += 1
-        backtrack(seatsLeft-minusSeat, peopleLeft-1, spaces, arr, i+minusSeat)
-        minusSeat = 1
-        arr[i] = False
-        backtrack(seatsLeft-minusSeat, peopleLeft, spaces, arr, i+minusSeat)
-    backtrack(seats, people, spaces, seatArr, 0)
-    return ans
+# ZHIQI's
+# def find_social_distance(seats, people, spaces):
+#     ans = 0    
+#     seatArr = [False] * seats
+#     def backtrack(seatsLeft, peopleLeft, spaces, arr, i):
+#         if peopleLeft == 0:
+#             nonlocal ans
+#             ans += 1
+#             return
+#         if seatsLeft <= 0:
+#             return
+#         minusSeat = 1
+#         arr[i] = True
+#         for j in range(i+1, i+1+spaces):
+#             if j < seats:
+#                 arr[j] = False
+#                 minusSeat += 1
+#         backtrack(seatsLeft-minusSeat, peopleLeft-1, spaces, arr, i+minusSeat)
+#         minusSeat = 1
+#         arr[i] = False
+#         backtrack(seatsLeft-minusSeat, peopleLeft, spaces, arr, i+minusSeat)
+#     backtrack(seats, people, spaces, seatArr, 0)
+#     return ans
+
+def find_social_distance(pax, seats, dist):
+	dp = [[0]*(seats+1) for i in range(pax+1)]
+	for i in range(pax+1):
+		for j in range(seats+1):
+			if i == 0:
+				dp[i][j] = 0
+			elif i == 1:
+				dp[i][j] = j
+			else:
+				if j < (i-1)*dist + i:
+					dp[i][j] = 0
+				else:
+					dp[i][j] = dp[i-1][j-dist-1] + dp[i][j-1]
+	return dp[-1][-1]
