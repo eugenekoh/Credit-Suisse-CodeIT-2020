1e8a5e7af586b9f9e95b34afdbd0cdb902196238
hanzhe <sith0008@e.ntu.edu.sg>
2020-09-26 14:09:12 +0800

    update endpoint

--- codeitsuisse/routes/clean_floor.py
+++ codeitsuisse/routes/clean_floor.py
@@ -7,7 +7,7 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-@app.route('/clean-floor', methods=['POST'])
+@app.route('/clean_floor', methods=['POST'])
 def evaluate_clean_floor():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
