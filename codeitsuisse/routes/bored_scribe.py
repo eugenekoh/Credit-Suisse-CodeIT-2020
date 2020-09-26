138484127a79f8c4b76a16ed844ed9044f12f197
hanzhe <sith0008@e.ntu.edu.sg>
2020-09-27 03:29:38 +0800

    revert enchant and add jsonify

--- codeitsuisse/routes/bored_scribe.py
+++ codeitsuisse/routes/bored_scribe.py
@@ -1,13 +1,12 @@
 import logging
 import json
-# import nltk
-# try:
-#     nltk.download("wordnet")
-# except:
-#     pass
-# from nltk.corpus import wordnet
-import enchant
-from flask import request
+import nltk
+try:
+    nltk.download("wordnet")
+except:
+    pass
+from nltk.corpus import wordnet
+from flask import request, jsonify
 
 from codeitsuisse import app
 import random
@@ -18,13 +17,13 @@ logger = logging.getLogger(__name__)
 def evaluate_bored_scribe():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    print(' ')
+
     results = []
     for test_case in data:
         message, count = decrypt(test_case['encryptedText'])
         results.append({"id": test_case["id"], "encryptionCount": count, "originalText": message})
     logging.info("result :{}".format(results))
-    return json.dumps(results)
+    return jsonify(results)
 
 def decrypt(message):
     start, end, count = getShift(message)
@@ -56,18 +55,17 @@ def decrypt(message):
         if dist_to_message is not None:
             candidates_filtered[cand] = dist_to_message
     
-    # l = [cand for cand in candidates_filtered]
-    # if len(l) == 0:
-    #     return message, 0
-    # rng = random.randint(0,len(l)-1)
-    # return l[rng], candidates_filtered[l[rng]]
+    l = [cand for cand in candidates_filtered]
+    if len(l) == 0:
+        return message, 0
+    rng = random.randint(0,len(l)-1)
+    return l[rng], candidates_filtered[l[rng]]
     # print(len(candidates_filtered),candidates_filtered)
-    d = enchant.Dict("en_US")
-    for cand, cand_count in candidates_filtered.items():
-        if wordBreak(cand, d):
-            return cand, cand_count
 
-    return message, 0 
+    # final_cand = None
+    # for cand, _ in candidates_filtered.items():
+    #     if wordBreak2(cand):
+    #         final_cand = cand
     # if final_cand == None:
     #     return message, 0
     # tmp = final_cand
@@ -140,22 +138,22 @@ def wordBreak(s, wordDict):
                         break
     return dp[0][-1]
 
-# def wordBreak2(s):
-#     dp = [[False]*len(s) for i in range(len(s))]
-#     for i in range(len(s)-1,-1,-1):
-#         for j in range(len(s)):
-#             if i == j:
-#                 dp[i][j] = True if (s[i] == 'a' or s[i] == 'i') else False
-#             elif i > j:
-#                 continue
-#             elif len(wordnet.synsets(s[i:j+1])) > 0:
-#                 dp[i][j] = True
-#             else:
-#                 for k in range(i,j+1):
-#                     if dp[i][k] and dp[k+1][j]:
-#                         dp[i][j] = True
-#                         break
-#     return dp[0][-1]
+def wordBreak2(s):
+    dp = [[False]*len(s) for i in range(len(s))]
+    for i in range(len(s)-1,-1,-1):
+        for j in range(len(s)):
+            if i == j:
+                dp[i][j] = True if (s[i] == 'a' or s[i] == 'i') else False
+            elif i > j:
+                continue
+            elif len(wordnet.synsets(s[i:j+1])) > 0:
+                dp[i][j] = True
+            else:
+                for k in range(i,j+1):
+                    if dp[i][k] and dp[k+1][j]:
+                        dp[i][j] = True
+                        break
+    return dp[0][-1]
 
 # d = enchant.Dict("en_US")
 # print(decrypt("oxzbzxofpxkbkdifpemxifkaoljb"))
