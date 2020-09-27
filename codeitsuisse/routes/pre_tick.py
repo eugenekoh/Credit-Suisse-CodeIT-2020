4d5d3ddcdca68f0510e678d690c2d4f296097b20
hanzhe <sith0008@e.ntu.edu.sg>
2020-09-27 08:24:17 +0800

    parse csv

--- codeitsuisse/routes/pre_tick.py
+++ codeitsuisse/routes/pre_tick.py
@@ -1,14 +1,25 @@
 import logging
 from flask import request, jsonify
 from codeitsuisse import app
+import io
+# from keras.models import Sequential, Model
+# from keras.layers import Dense, Embedding, LSTM, Dropout, Input, concatenate
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 
 
 @app.route('/pre-tick', methods=['POST'])
 def evaluate_pre_tick():
-    data = request.get_json()
+    data = request.data
     logger.info(f"data: {data}")
     result = {}
-
+    df = pd.read_csv(io.StringIO(data.decode('utf-8')))
+    train(df)
     return jsonify(result)
+
+
+def train(df):
+    print(df.cols)
+    return 
+# def predict()
