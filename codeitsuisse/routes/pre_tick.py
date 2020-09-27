c3065143b32696634ec4b112900e06f3a640f989
hanzhe <sith0008@e.ntu.edu.sg>
2020-09-27 08:54:46 +0800

    pre_tick test

--- codeitsuisse/routes/pre_tick.py
+++ codeitsuisse/routes/pre_tick.py
@@ -1,10 +1,10 @@
 import logging
 from flask import request, jsonify
 from codeitsuisse import app
-import io
-# from keras.models import Sequential, Model
-# from keras.layers import Dense, Embedding, LSTM, Dropout, Input, concatenate
+from io import StringIO
+
 import pandas as pd
+import lightgbm as lgb
 
 logger = logging.getLogger(__name__)
 
@@ -13,16 +13,41 @@ logger = logging.getLogger(__name__)
 def evaluate_pre_tick():
     data = request.data
     logger.info(f"data: {data}")
-    result = {data}
-    df = pd.read_csv(io.StringIO(data.decode('utf-8')))
-    train(df)
-    return jsonify(result)
+    s = str(data,'utf-8')
+    data = StringIO(s) 
+    df = pd.read_csv(data)
+    model = train(df)
+    X = df[-1:][['Open','High','Low','Volume']].values
+    Y = predict(model, X)[0]
+    return Y
 
 
 def train(df):
-    print(df['Open'].values)
+    train = df[:1600]
+    val = df[1600:]
+    x_train = train[['Open','High','Low','Volume']]
+    y_train = train.Close.values
+    x_val = val[['Open','High','Low','Volume']]
+    y_val = val.Close.values
+    lgb_params = {
+        'boosting_type': 'gbdt',         
+        'objective': 'regression',       
+        'metric': ['rmse'],             
+        'subsample': 0.6,                
+        'subsample_freq': 1,
+        'learning_rate': 0.05,           
+        'num_leaves': 2**9,            
+        'min_data_in_leaf': 8,      
+        'feature_fraction': 0.6,
+        'n_estimators': 20,            
+        'early_stopping_rounds': 30,     
+        'verbose': -1,
+    } 
+    train_set = lgb.Dataset(x_train, y_train)
+    val_set = lgb.Dataset(x_val, y_val)
+    lgb_model = lgb.train(lgb_params, train_set, num_boost_round = 500, valid_sets = [train_set, val_set], verbose_eval = 100)
 
-    # x_train = df[['Open','High','Low','Volume']].values
-    # y_train = df[['Close']]
-    return 
-# def predict()
+    return lgb_model
+
+def predict(model, features):
+    return model.predict(features)
